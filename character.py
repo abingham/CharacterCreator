21e696b3f7ebf49c4fee918791a2d404c9ec6fdd
ambingham <navylucky78@gmail.com>
2019-03-06 19:35:18 -0500

    Update character.py

--- character.py
+++ character.py
@@ -3,10 +3,13 @@ from sys import exit
 # This is an attempt to create a D&D Character creator.
 # The first rendition will create a Human Fighter from Fifth Edition Rules.
 
-def start:
-  print "Choose a race from the following:"
-        "Human"
-        "Dwarf"
-        "Elf"
+def start():
+  print: "Choose a race from the following:"
+  print: "Human"
+  print: "Dwarf"
+  print: "Elf"
+    
+  choice = raw_input(">")
+  
 
 start()
