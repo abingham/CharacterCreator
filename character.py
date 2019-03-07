8daaec00dfad3f11b85206da61a9b3d5756763ff
ambingham <navylucky78@gmail.com>
2019-03-06 19:42:32 -0500

    Update character.py

--- character.py
+++ character.py
@@ -4,16 +4,21 @@ from sys import exit
 # The first rendition will create a Human Fighter from Fifth Edition Rules.
 
 def start():
-  print: "Choose a race from the following:"
-  print: "Human"
-  print: "Dwarf"
-  print: "Elf"
+  print "Choose a race from the following:"
+  print "Human"
+  print "Dwarf"
+  print "Elf"
     
   choice = raw_input(">")
   
-  if choice == Human
-  elif choice == Dwarf
-  elif choice == Elf
+  if choice == "Human":
+    Human()
+  elif choice == "Dwarf":
+    Dwarf()
+  elif choice == "Elf":
+    Elf()
+  else:
+    start()
   
 
 start()
